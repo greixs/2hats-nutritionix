bedd83f13cc1c4cd18470d78e4fdc130c504690e
frenox <greixs@gmail.com>
2019-11-08 18:25:38 +1100

    Updates

---
+++ precache-manifest.56f0349a505b7743368bc0d421539ec7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ae7afc6824915111f7b938dc4fee132",
+    "url": "/2hats-nutritionix/index.html"
+  },
+  {
+    "revision": "117ee22e357279401094",
+    "url": "/2hats-nutritionix/static/css/main.b3473eaa.chunk.css"
+  },
+  {
+    "revision": "c2d7e5c97a4e92382222",
+    "url": "/2hats-nutritionix/static/js/2.c63bf107.chunk.js"
+  },
+  {
+    "revision": "117ee22e357279401094",
+    "url": "/2hats-nutritionix/static/js/main.4eeebd6e.chunk.js"
+  },
+  {
+    "revision": "3f66d3e88e7f31939858",
+    "url": "/2hats-nutritionix/static/js/runtime-main.615864a3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/2hats-nutritionix/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/2hats-react/precache-manifest.844d4f777287682d1ddbbece50c24b92.js"
+  "/2hats-nutritionix/precache-manifest.56f0349a505b7743368bc0d421539ec7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/2hats-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/2hats-nutritionix/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
